88ba348c1d7ca485a1c6ddbebef845b0ff498061
unknown <deepaksoun12@gmail.com>
2026-06-29 15:40:47 +0530

    Created authentication controller

---
+++ backend/controllers/authController.js
@@ -0,0 +1,45 @@
+import User from "../models/User.js";
+
+// Register User
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // Check required fields
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
+    }
+
+    // Check if user already exists
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exists",
+      });
+    }
+
+    // Create new user
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+      user,
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
