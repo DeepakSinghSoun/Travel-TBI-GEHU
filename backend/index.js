29064dfa280bd0f699831aad2c9ce0314bc9b9df
unknown <deepaksoun12@gmail.com>
2026-06-29 15:37:12 +0530

    Created User model

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,44 @@
+import dns from "dns";
+dns.setDefaultResultOrder("ipv4first");
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 
-import dns from "dns";
-dns.setDefaultResultOrder("ipv4first");
-dns.setServers(["1.1.1.1", "8.8.8.8"]);
-
 dotenv.config();
-
-const app = express();
 
 // Connect MongoDB
 connectDB();
+
+const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-// Test Route
+// Home Route
 app.get("/", (req, res) => {
-  res.json({
-    message: "Backend is running successfully 🚀",
+  res.status(200).json({
+    success: true,
+    message: "Travel TBI-GEHU Backend API is Running 🚀",
   });
 });
 
+// Health Check Route
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    success: true,
+    status: "OK",
+    uptime: process.uptime(),
+    timestamp: new Date(),
+  });
+});
+
+// Start Server
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
