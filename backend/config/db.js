11c33f85414f4bedf202a711fcb0c108669b16ba
unknown <deepaksoun12@gmail.com>
2026-06-29 15:31:27 +0530

    Configured MongoDB database connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,13 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log("MongoDB Connected");
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log("✅ MongoDB Connected Successfully");
   } catch (error) {
-    console.log(error.message);
+    console.error("❌ MongoDB Connection Failed");
+    console.error(error.message);
+    process.exit(1);
   }
 };
 
